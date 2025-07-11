f7e96f57f921d25726e290dcf2d0982b1f5e2618
DinushaR <dinushakln@gmail.com>
2025-07-10 23:03:15 -0300

    modified:   pom.xml modified:   src/main/java/com/example/RootController.java

--- src/main/java/com/example/RootController.java
+++ src/main/java/com/example/RootController.java
@@ -3,6 +3,7 @@ package com.example;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.Produces;
 
 @Controller("/")
 public class RootController {
